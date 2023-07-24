889444cda128a585d8783c397c7c9c1610770490
Alheli Cabrera <alecab1308@gmail.com>
2023-07-24 10:36:15 -0600

    children coming from the lazyLoad paths.

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 //localhost:4200/heroes
 const routes: Routes = [
@@ -10,8 +13,11 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'new-hero', component: NewHeroPageComponent },
-      {  },
-      {  }
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewHeroPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list' }
     ]
   },
 ];
