6017c123d6c0894f5ea00dc641bf5e9e6f46d151
Alheli Cabrera <alecab1308@gmail.com>
2023-07-24 09:50:01 -0600

    components creation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HeroPageComponent } from './hero/pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './hero/pages/layout-page/layout-page.component';
+import { ListPageComponent } from './hero/pages/list-page/list-page.component';
+import { NewPageComponent } from './hero/pages/new-page/new-page.component';
+import { SearchPageComponent } from './hero/pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
