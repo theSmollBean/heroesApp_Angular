c65b99a1b5dc79c05f8beda3f58057402969fee0
Alheli Cabrera <alecab1308@gmail.com>
2023-07-27 09:33:16 -0600

    last commit with guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanMatch } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import( './auth/auth.module' ).then( m => m.AuthModule ),
+    canActivate: [ AuthGuard ],
+    canMatch: [ AuthGuard ]
   },
   {
     path: 'heroes',
     loadChildren: () => import( './heroes/heroes.module' ).then( m => m.HeroesModule ),
+    canActivate: [ AuthGuard ],
+    canMatch: [ AuthGuard ]
   },
   {
     path: '404',
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from './../../../auth/services/auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -11,5 +14,21 @@ export class LayoutPageComponent {
     { label: 'Listado', icon: 'label', url: './list'},
     { label: 'Añadir', icon: 'add', url: './new-hero'},
     { label: 'Buscar', icon: 'search', url: './search'},
-  ]
+  ];
+
+  
+
+  constructor(
+    private authService: AuthService,
+    private router: Router){}
+
+  get currentUser(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+
+  }
 }
